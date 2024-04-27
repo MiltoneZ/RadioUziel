af276aeade9075d6db9d7ae6aec0b81385272a15
MiltoneZ <milton.uzieldm@gmail.com>
2024-04-26 18:58:02 -0600

    Clases definidas.

--- src/com/aluracursos/radiouziel/modelos/Audio.java
+++ src/com/aluracursos/radiouziel/modelos/Audio.java
@@ -1,5 +1,48 @@
 package com.aluracursos.radiouziel.modelos;
 
 public class Audio {
+    private String titulo;
+    private int totalDeRepodrucciones;
+    private int totalDeMegusta;
+    private int clasificacion;
 
+    public void meGusta(){
+        this.totalDeMegusta++;
+    }
+
+    public void reproduce(){
+        this.totalDeRepodrucciones++;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalDeRepodrucciones() {
+        return totalDeRepodrucciones;
+    }
+
+    public void setTotalDeRepodrucciones(int totalDeRepodrucciones) {
+        this.totalDeRepodrucciones = totalDeRepodrucciones;
+    }
+
+    public int getTotalDeMegusta() {
+        return totalDeMegusta;
+    }
+
+    public void setTotalDeMegusta(int totalDeMegusta) {
+        this.totalDeMegusta = totalDeMegusta;
+    }
+
+    public int getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(int clasificacion) {
+        this.clasificacion = clasificacion;
+    }
 }
